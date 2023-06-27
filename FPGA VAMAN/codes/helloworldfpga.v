44f87b9de2a1b1b7f31ca1a9948c884f28c56527
Vamsichowdary04 <117671618+Vamsichowdary04@users.noreply.github.com>
2023-06-27 15:04:02 +0530

    Update helloworldfpga.v

--- FPGA VAMAN/codes/helloworldfpga.v
+++ FPGA VAMAN/codes/helloworldfpga.v
@@ -1,10 +1,4 @@
-/*Program for blinking LED
-January 16,2021
-Code by G V V Sharma
-Released under GNU GPL
-*/
 
-//declaring the blink module
 module helloworldfpga(
     input wire Q,
     input wire R,
